9b208713a887ec9d14a9ffeb98737556084f85ae
UlrichDevp <ulrichsoh72@gmail.com>
2025-05-16 14:05:34 +0100

    feat: UpdateEvent

--- Journalisation dévénements/controllers/eventController.js
+++ Journalisation dévénements/controllers/eventController.js
@@ -86,7 +86,47 @@ const eventController = {
                             res.status(200).send(findContact[0])
                         }else{res.status(404).send({msg: "invalid id"})}
                     })
-    }
+    },
+
+    updateEvent: (req, res) =>{ 
+            
+                    fs.readFile(databsejsonPath, "utf-8", (err, data)=>{
+                        if(err) throw err;
+                
+                        let database = JSON.parse(data);
+            
+                        const {name, date, origin} = req.body
+                        const {id} = req.params
+            
+                        let findContact = database.map(item=>{
+                            if(item.id == id){
+                                name? item.name = name : item.name = item.name;
+                                date? item.date = date : item.date = item.date;
+                                origin? item.origin = origin : item.origin = item.origin;
+                                return item;
+                            }else{return null}
+                        }).filter(item => item !== null);
+
+                        if(findContact.length>0){
+                            logStream.write("Event updated " + Date() + "\n", () => {
+                    console.log('Write completed');
+                    });
+                            res.status(200).send({msg: "Updated successfuly"})
+                        }else{
+                            logStream.write("Event are not added " + Date() + "\n", () => {
+                    console.log('Write completed, do more writes now.');
+                    });
+                            res.status(404).send({msg: "invalid id"})
+                        }
+            
+                        fs.writeFile(databsejsonPath, JSON.stringify(database,null,2) , (err, data)=>{
+                            if (err) throw err;
+                        })
+                        fs.writeFile(databsecsvPath, JSON.stringify(database) , (err, data)=>{
+                            if (err) throw err;
+                        })
+                    })
+        }
 }
 
 export default eventController
