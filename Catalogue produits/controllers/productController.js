0b4a72a259867595ac1cf477434e4a49de90b74d
UlrichDevp <ulrichsoh72@gmail.com>
2025-05-17 12:31:00 +0100

    feat: 90%exo4

--- Catalogue produits/controllers/productController.js
+++ Catalogue produits/controllers/productController.js
@@ -1,12 +1,137 @@
 import fs from "fs";
 import { randomUUID } from "crypto";
 
-const databsejsonPath = "../database.json";
-const databsecsvPath = "../database.csv";
+const databsejsonPath = "./Catalogue produits/database.json";
+const databsecsvPath = "./Catalogue produits/database.csv";
 
 const productController = {
 
-    createproduct: 1,
+    createProduct: (req, res)=>{
+                
+                const {name, price, promo} = req.body;
+                if(!name || !price || !promo){
+                    res.status(400).send({msg: "Creation failure: That informations are not complete"})
+                } else{
+        
+                    let product = {
+                        "id" : randomUUID(),
+                        "name": name,
+                        "price": price,
+                        "promo": promo
+                    } // Ajout de l'id
+        
+                    fs.readFile(databsejsonPath , "utf-8", (err, data)=>{
+                    if(err) throw err;
+            
+                    let database = JSON.parse(data);
+                    
+                    database.push(product);
+            
+                    fs.writeFile(databsejsonPath , JSON.stringify(database,null,2) , (err, data)=>{
+                        if (err) throw err;
+                    });
+                    fs.writeFile(databsecsvPath, JSON.stringify(database) , (err, data)=>{
+                        if (err) throw err;
+                    })
+                })
+                res.status(201).send({msg: "Created Successfully"})
+                }
+            },
+    
+            getAllProducts: (req,res)=>{
+                    fs.readFile(databsejsonPath, "utf8", (err, data) => {
+                        if(err) throw err;
+    
+                        const database = JSON.parse(data);
+                        res.status(200).json(database);
+                    })
+            },
+    
+            getProductById: (req,res)=>{
+    
+                    const {id} = req.params;
+            
+                    fs.readFile(databsejsonPath, "utf8", (err, data) => {
+                        if(err) throw err;
+                        const database = JSON.parse(data);
+            
+                        let findProduct = database.map(item=>{
+
+                            if(item.id == id){
+                                return item;
+                            }else{return null}
+                        }).filter(item => item !== null);
+                        if(findProduct.length>0){
+                            res.status(200).send(findProduct[0])
+                        }else{res.status(404).send({msg: "invalid id"})}
+                    })
+            },
+    
+            updateProduct: (req, res) =>{ 
+            
+                    fs.readFile(databsejsonPath, "utf-8", (err, data)=>{
+                        if(err) throw err;
+                
+                        let database = JSON.parse(data);
+            
+                        const {name, price, promo} = req.body
+                        const {id} = req.params
+            
+                        let findProduct = database.map(item=>{
+                            if(item.id == id){
+                                name? item.name = name : item.name = item.name;
+                                price? item.price = price : item.price = item.price;
+                                promo? item.promo = promo : item.promo = item.promo;
+                                return item;
+                            }else{return null}
+                        }).filter(item => item !== null);
+                        if(findProduct.length>0){
+                            res.status(200).send({msg: "Updated successfuly"})
+                        }else{res.status(404).send({msg: "Not Found:invalid id"})}
+            
+                        fs.writeFile(databsejsonPath, JSON.stringify(database,null,2) , (err, data)=>{
+                            if (err) throw err;
+                        })
+                        fs.writeFile(databsecsvPath, JSON.stringify(database) , (err, data)=>{
+                            if (err) throw err;
+                        })
+                    })
+                },
+    
+            deleteProduct: (req, res) =>{ 
+            
+                    fs.readFile(databsejsonPath, "utf-8", (err, data)=>{
+                        if(err) throw err;
+                
+                        let database = JSON.parse(data);
+            
+                        const {id} = req.params
+                        let findProduct = database.map((item,index)=>{
+                            if(item.id == id){
+                                return index;
+    
+                            }else{
+                                return null
+                            }
+                        }).filter(item => item !== null);
+                        if(findProduct.length>0){
+                            database.splice(findProduct,1)
+                            res.status(200).send({msg: "product deleted"})
+                        }else{res.status(404).send({msg: "invalid id"})}
+            
+                        fs.writeFile(databsejsonPath, JSON.stringify(database,null,2) , (err, data)=>{
+                            if (err) throw err;
+                        })
+                        fs.writeFile(databsecsvPath, JSON.stringify(database) , (err, data)=>{
+                            if (err) throw err;
+                        })
+                    })
+            },
+
+            getProductsWithPromos: (req, res) => {
+                
+            }
+
 }
 
 export default productController
--- Catalogue produits/routes/productRoutes.js
+++ Catalogue produits/routes/productRoutes.js
@@ -4,6 +4,12 @@ const router = Router();
 
 import productController from "../controllers/productController.js";
 
+router.post("/products", productController.createProduct);
+router.get("/products", productController.getAllProducts);
+router.get("/products/:id", productController.getProductById);
+router.put("/products/:id", productController.updateProduct);
+router.delete("/products/:id", productController.deleteProduct)
+router.get('/products/promos', productController.getProductsWithPromos);
 
 
 export default router;
